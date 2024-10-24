a879531377e86f3afd7f96b29d2b29e61fd74787
sabrinacouto <sabrinacoutolima@gmail.com>
2024-10-24 00:16:27 -0300

    adicionando swagger

---
+++ checkpoint02-java-hotel/src/main/java/com/example/checkpoint02_java_hotel/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.example.checkpoint02_java_hotel.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .packagesToScan("checkpoint02_java_hotel.Controller")
+                .build();
+    }
+}
